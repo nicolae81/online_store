88e1c0da6a9eaa2c365a8031d316aaf2525545f0
Nick <51345371+nicolae81@users.noreply.github.com>
2024-01-16 12:59:36 +0200

    Update settings.py

--- online_store/settings.py
+++ online_store/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'online_store.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'BASE_DIR / 'db.sqlite3'',
+        'NAME': BASE_DIR / 'db.sqlite3',
         'USER': 'oberondjango',
         'PASSWORD': 'your_database_password',
         'HOST': 'oberondjango.mysql.eu.pythonanywhere-services.com',  # For PythonAnywhere, this is typically 'your_username.mysql.pythonanywhere-services.com'
