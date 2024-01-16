af2a3e026710980d12d8cb90ce381594a04d6d47
Nick <51345371+nicolae81@users.noreply.github.com>
2024-01-16 12:58:51 +0200

    Update settings.py

--- online_store/settings.py
+++ online_store/settings.py
@@ -80,12 +80,25 @@ WSGI_APPLICATION = 'online_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'BASE_DIR / 'db.sqlite3'',
+        'USER': 'oberondjango',
+        'PASSWORD': 'your_database_password',
+        'HOST': 'oberondjango.mysql.eu.pythonanywhere-services.com',  # For PythonAnywhere, this is typically 'your_username.mysql.pythonanywhere-services.com'
+        'PORT': '3306',  # For MySQL, the default is '3306'
     }
 }
+
+
 
 # DATABASES = {
 #     "default": {
